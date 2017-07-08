f495b0fdfcdb8e4f8f0baf2c5f7d419197bdd5d6
StanislavMayorov <Stanislav@fksis.by>
2017-07-08 21:51:53 +0300

    feature: add webpack config

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+require('./styles.css');
+require('tether');
+require('jquery');
+require('bootstrap');
+require( 'bootstrap/dist/css/bootstrap.css');
+
+debugger;
+
+$(function () {
+    console.log('jquery works');
+});
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+"use strict";
+const webpack = require('webpack');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "index.js"),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+                use: [{
+                    loader: "file-loader"
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.styles.css'),
+        new webpack.ProvidePlugin({
+            // inject ES5 modules as global vars
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Tether: 'tether'
+        })
+    ]
+};
